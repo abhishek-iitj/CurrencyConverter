2fb83d7742d1362f148a53771601a7c9aa257b08
Abhishek Sah <abhisheksah@Abhisheks-MacBook-Pro.local>
2019-07-18 16:36:43 +0530

    Adds currency multiplier test

--- src/test/java/MultiplicationTest.java
+++ src/test/java/MultiplicationTest.java
@@ -1,2 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class MultiplicationTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        Assert.assertEquals(10, five.amount);
+    }
 }
