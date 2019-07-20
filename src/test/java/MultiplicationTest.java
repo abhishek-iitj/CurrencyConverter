947d39dc68d1caa59bbd8438186b5fc3672d6d4f
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-21 00:51:58 +0530

    Eliminates redundant test

--- src/test/java/MultiplicationTest.java
+++ src/test/java/MultiplicationTest.java
@@ -14,8 +14,6 @@ public class MultiplicationTest {
     public void testEquality(){
         Assert.assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         Assert.assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        Assert.assertTrue(Money.franc(5).equals(Money.franc(5)));
-        Assert.assertFalse(Money.franc(5).equals(Money.franc(6)));
         Assert.assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
