957e819ed5aa937bb0ec2debd79f4b6f976aafe4
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-21 00:13:11 +0530

    Pushes identical constructor implementation from base to parent class

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,7 +1,6 @@
 public class Dollar extends Money{
     Dollar(int amount, String currency){
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
     Money times(int multiplier){
         return Money.dollar(amount * multiplier);
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -3,6 +3,10 @@ abstract class Money {
     protected String currency;
     abstract Money times(int multiplier);
 
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
     static Money dollar (int amount){
         return new Dollar(amount, "USD");
     }
