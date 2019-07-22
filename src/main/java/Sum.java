15670f61fcbaa8c6c1a3b01f981184b33648c44f
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-22 17:46:25 +0530

    Refactors two level of object referencing

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -5,4 +5,8 @@ public class Sum implements MoneyExpression{
         this.augend = augend;
         this.addend = addend;
     }
+    public Money reduce(String to){
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
 }
